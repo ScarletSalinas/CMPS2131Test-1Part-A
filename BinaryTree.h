defc183aab8993d001bcd1bb5bcbac88c372f49f
Scarlet Salinas <2022156127@ub.edu.bz>
2025-03-31 16:38:18 -0600

    added bst class

--- BinaryTree.h
+++ BinaryTree.h
@@ -1,16 +1,12 @@
 #ifndef BINARYTREE_H
 #define BINARYTEE_H
-
-#include <iostream>
-using namespace std;
-
 struct BinaryTreeNode {
     int value;
     BinaryTreeNode* left;
     BinaryTreeNode* right;
 
     // Const.
-    BinaryTreeNode(const int value) : value(value), left(nullptr), right(nullptr) {}
+    explicit BinaryTreeNode(const int val) : value(val), left(nullptr), right(nullptr) {}
 
 };
 
@@ -19,21 +15,32 @@ class BinarySearchTree {
         BinarySearchTree(); // Const.
         ~BinarySearchTree(); // Dest.
 
+        // Public interface
         void insert(int value);
-        bool search(int value);
-        void delete(int value);
-        void preOrderTravarsal();
-        void inOrderTraversla();
-        void postOrderTraversal();
-        void levelOrderTraveral();
-        int getCount();
-        int getTreeHeight();
+        bool search(int value) const;
+        void remove(int value);
+        void preOrderTraversal() const;
+        void inOrderTraversal() const;
+        void postOrderTraversal() const;
+        void levelOrderTraversal() const;
+        int getCount() const;
+        int getHeight() const;
     
     private:
-        BinarySearchTree* root;
-        
+        BinaryTreeNode* root;
 
+        // Helper funcs
+        BinaryTreeNode* insertHelper(BinaryTreeNode* node, int value);
+        BinaryTreeNode* removeHelper(BinaryTreeNode* node, int value);
+        bool searchHelper(BinaryTreeNode* node, int val) const;
+        void preOrderHelper(BinaryTreeNode* node) const;
+        void inOrderHelper(BinaryTreeNode* node) const;
+        void postOrderHelper(BinaryTreeNode* node) const;
+        void levelOrderHelper(BinaryTreeNode* node) const;
+        int treeCount(BinaryTreeNode* node) const;
+        int treeHeight(BinaryTreeNode* node) const;
+        void destroyTree(BinaryTreeNode* node);
 
-}
+};
 
 #endif
