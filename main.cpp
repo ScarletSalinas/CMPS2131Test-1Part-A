96df8804effea03edf9aebfd12cf4f7a3c40e518
Scarlet <112662775+ScarletSalinas@users.noreply.github.com>
2025-04-01 09:11:48 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main() {
 
     BinarySearchTree tree;
+
+    // Call insert method to insert values into tree
     tree.insert(50);
     tree.insert(30);
     tree.insert(70);
@@ -12,10 +14,12 @@ int main() {
     tree.insert(40);
     tree.insert(60);
     tree.insert(80);
-
+    
     cout << "\nlevel order traversal:\n";
     tree.levelOrderTraversal();
+    
     tree.remove(70);
+    
     cout << "\nlevel order traversalfter deleting (40):\n ";
     tree.levelOrderTraversal();
 
